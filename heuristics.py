67ee5387df65a7885adf5c7f88e67762d0f95b81
amitprp <amitprp@gmail.com>
2024-01-21 18:11:30 +0200

    Finished base_heuristic

--- heuristics.py
+++ heuristics.py
@@ -1,7 +1,18 @@
 
 
 def base_heuristic(_pancake_state):
-    return 0
+    pancakesList = _pancake_state.get_state_str().split(',')
+    num_of_pancakes = len(pancakesList)
+    InOrder = False
+    sum = 0
+    for pancake in pancakesList:
+        if int(pancake) != num_of_pancakes:
+            InOrder = True
+        if InOrder:
+            sum += int(pancake)
+        num_of_pancakes -= 1
+
+    return sum
 
 def advanced_heuristic(_pancake_state):
     return 0
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     goal_state = "4,3,2,1"
     pancake_input = "4,2,3,1"
     pancake_state = pancake_state(pancake_input)
-    search_result = search(pancake_state, base_heuristic, goal_state)
+    print(base_heuristic(pancake_state))
+    # search_result = search(pancake_state, base_heuristic, goal_state)
 
 
 
