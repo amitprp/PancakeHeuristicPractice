63695667b47dd0278be57a4b65a6a4e50217ed40
kobiza1 <kobiza@post.bgu.ac.il>
2024-01-21 19:39:20 +0200

    Finished get_neighbours

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from search import search
 from pancake_state import pancake_state
 from heuristics import *
 
+
 if __name__ == '__main__':
     goal_state = "4,3,2,1"
-    pancake_input = "4,2,3,1"
+    pancake_input = "4,3,2,1"
     pancake_state = pancake_state(pancake_input)
     print(base_heuristic(pancake_state))
-    # search_result = search(pancake_state, base_heuristic, goal_state)
-
-
+    search_result = search(pancake_state, base_heuristic, goal_state)
 
--- pancake_state.py
+++ pancake_state.py
@@ -4,12 +4,19 @@ class pancake_state:
 
     def __init__(self, state_str):
         self.state_str = state_str
+
         #you may add data stractures to improve the search
 
 
     #returns an array of tuples of neighbor states and the cost to reach them: [(pancake_state1, cost1), (pancake _state2, cost2)...]
     def get_neighbors(self):
-        pass
+        neighbors = []
+        state_len = len(self.state_str)-1
+        commas = state_len//2
+        for i in range(state_len-commas):
+            ps = pancake_state(self.rotate_state(i*2))
+            neighbors += [(ps,1)]
+        return neighbors
 
 
     #you can change the body of the function if you want
@@ -23,3 +30,10 @@ class pancake_state:
 
     def get_state_str(self):
         return self.state_str
+
+    def rotate_state(self, index1):
+        to_rotate = self.state_str;
+        rotated_part =  to_rotate[index1:]
+        return  to_rotate[:index1] + rotated_part[::-1]
+
+
